7df2b6ff6a6a05bc3c13de084fd4086bd1a55c32
root <oabuoun1>
2017-12-15 12:07:04 +0000

    running node-exporter at the end

--- jqueuing_manager.sh
+++ jqueuing_manager.sh
@@ -4,7 +4,7 @@ echo $WORKERS
 for WORKER in $WORKERS
 do
 	IP=$(ssh -C -x -q -o ConnectTimeout=2 -o StrictHostKeyChecking=no cloudsigma@$1 "sudo docker node inspect $WORKER --format '{{ .Status.Addr  }}'")
-	echo ========================= $IP =================================
+	echo ========================= $WORKER === $IP =================================
 	OUTPUT=$(ssh -C -x -q -o ConnectTimeout=5 -o StrictHostKeyChecking=no  cloudsigma@$IP "sudo docker container logs jqueuing_manager_$IP")
 	echo $OUTPUT
 done
