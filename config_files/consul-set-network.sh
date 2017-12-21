6bf017ae81e32f084f5efe7ec131a34dc480bfcc
Osama -Linux-OfficePC <osabuoun>
2017-12-21 14:09:13 +0000

    adding content as files

--- config_files/consul-set-network.sh
+++ config_files/consul-set-network.sh
@@ -1,1 +1,12 @@
-d
+#!/bin/bash
+echo "Setup NETWORK starts."
+myhost=`hostname`
+ipaddress=`ifconfig | awk '/inet addr/{print substr($2,6)}' | grep -v 127.0.0.1 | head -n 1`
+cp /etc/hosts /etc/hosts.old
+grep -v "$myhost" /etc/hosts.old > /etc/hosts
+
+echo "IPADDRESS: $ipaddress"
+echo "$ipaddress $myhost" >> /etc/hosts
+
+rm -rf /etc/resolvconf/*
+echo "Setup NETWORK finished."
